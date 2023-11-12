1860f6385b214c6a2159ce025a1f26c36a048038
richmr <richmr@users.noreply.github.com>
2023-11-11 17:06:29 -0800

    Added hide input switch

--- src/winhotkey/cli/__init__.py
+++ src/winhotkey/cli/__init__.py
@@ -12,13 +12,12 @@ cli_app = typer.Typer()
 
 @cli_app.command()
 def run(hotkey_prefix: Annotated[str, typer.Option(help="'keyboard' compatible hot key prefix'")] = "ctrl+alt",
-        reverse_hotkey_order: Annotated[bool, typer.Option(help="Start with 1 and go to 0 or 0 and go to 1.  I actually prefer 0 to 1")] = True):
+        reverse_hotkey_order: Annotated[bool, typer.Option(help="Start with 1 and go to 0 or 0 and go to 1.  I actually prefer 0 to 1")] = True,
+        hide_input: Annotated[bool, typer.Option(help="In case your 'phrases' are sensitive, you will not see what you enter")] = True):
     """
     Allows you store up to 10 'phrases' for instant recall by typing 'Control+Alt+[1-0]'\n
-    In case your 'phrases' are sensitive, you will not see what you enter\n
     Just hit enter to stop storing 'phrases'\n
     """
-    done = False
     possible_entries = [f"{i}" for i in range(1,10)]
     possible_entries.append("0")
     if reverse_hotkey_order:
@@ -26,7 +25,7 @@ def run(hotkey_prefix: Annotated[str, typer.Option(help="'keyboard' compatible h
     keys_to_add = {}
     for entry in possible_entries:
         key = f'{hotkey_prefix}+{entry}'
-        phrase = typer.prompt(f"Please enter phrase for '{key}'", hide_input=True, default="Press Enter when done")
+        phrase = typer.prompt(f"Please enter phrase for '{key}'", hide_input=hide_input, default="Press Enter when done")
         if phrase == "Press Enter when done":
             break
         keys_to_add[key] = phrase
