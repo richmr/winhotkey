146c02329123bb873157b6f901d05b70b2ed4fd0
richmr <richmr@users.noreply.github.com>
2023-11-11 17:01:42 -0800

    Basics in place

--- src/winhotkey/cli/__init__.py
+++ src/winhotkey/cli/__init__.py
@@ -1,12 +1,47 @@
 # SPDX-FileCopyrightText: 2023-present richmr <richmr@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
-import click
+import typer
+import keyboard
+from typing_extensions import Annotated
+import platform
 
 from winhotkey.__about__ import __version__
 
+cli_app = typer.Typer()
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
-@click.version_option(version=__version__, prog_name="winhotkey")
+@cli_app.command()
+def run(hotkey_prefix: Annotated[str, typer.Option(help="'keyboard' compatible hot key prefix'")] = "ctrl+alt",
+        reverse_hotkey_order: Annotated[bool, typer.Option(help="Start with 1 and go to 0 or 0 and go to 1.  I actually prefer 0 to 1")] = True):
+    """
+    Allows you store up to 10 'phrases' for instant recall by typing 'Control+Alt+[1-0]'\n
+    In case your 'phrases' are sensitive, you will not see what you enter\n
+    Just hit enter to stop storing 'phrases'\n
+    """
+    done = False
+    possible_entries = [f"{i}" for i in range(1,10)]
+    possible_entries.append("0")
+    if reverse_hotkey_order:
+        possible_entries = possible_entries[::-1]
+    keys_to_add = {}
+    for entry in possible_entries:
+        key = f'{hotkey_prefix}+{entry}'
+        phrase = typer.prompt(f"Please enter phrase for '{key}'", hide_input=True, default="Press Enter when done")
+        if phrase == "Press Enter when done":
+            break
+        keys_to_add[key] = phrase
+
+    for (n, (key_to_add, phrase)) in enumerate(keys_to_add.items()):
+        keyboard.add_hotkey(key_to_add, lambda: keyboard.write(phrase)) 
+    
+    print("Press ESC to exit.  Leave this window open!")
+
+    # Blocks until you press esc.
+    keyboard.wait('esc')
+
 def winhotkey():
-    click.echo("Hello world!")
+    if not platform.system() == "Windows":
+        print("This will only work on windows, sorry")
+        return
+    cli_app()
+
