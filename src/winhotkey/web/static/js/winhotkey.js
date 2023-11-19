83c40254d8b6644fea2942b7336cac245e43a5ef
Michael Rich <richmr2174@gmail.com>
2023-11-18 21:34:45 -0800

    Delay time control added to gui

--- src/winhotkey/web/static/js/winhotkey.js
+++ src/winhotkey/web/static/js/winhotkey.js
@@ -4,13 +4,14 @@ function delayType(hotkey, index) {
         index:index,
         delay:0.1
     };
+    current_delay = parseInt($("#delay_setting").val())
     $.toast({ 
         text : "Your phrase will type when this toast closes.\nPut focus on the target input box", 
         showHideTransition : 'slide',  // It can be plain, fade or slide
         bgColor : 'green',              // Background color for toast
         textColor : '#eee',            // text color
         allowToastClose : false,       // Show the close button or not
-        hideAfter : 3000,              // `false` to make it sticky or time in miliseconds to hide after
+        hideAfter : current_delay * 1000,              // `false` to make it sticky or time in miliseconds to hide after
         // stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
         textAlign : 'center',            // Alignment of text i.e. left, right, center
         position : 'top-center',       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
